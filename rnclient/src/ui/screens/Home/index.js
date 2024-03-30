b13671d32778a2280b4aa007dcd957f6dad39e40
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-03-30 16:44:14 +0530

    Phone signin screen added,

--- rnclient/src/ui/screens/Home/index.js
+++ rnclient/src/ui/screens/Home/index.js
@@ -2,13 +2,15 @@ import Text from '@ui/atoms/Text';
 import {useTranslation} from 'react-i18next';
 import {View} from 'react-native';
 import GoogleSignIn from '../google/GoogleSignIn';
+import PhoneSignIn from '../phone/PhoneSignIn';
 
 const Home = () => {
   const {t} = useTranslation();
 
   return (
     <View>
-      <GoogleSignIn />
+      {false && <GoogleSignIn />}
+      {true && <PhoneSignIn />}
     </View>
   );
 };
