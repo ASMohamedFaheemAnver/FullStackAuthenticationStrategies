2fb785ea1c7fc862a5e141e7665ad3f4fc2a58a8
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-05-31 17:08:16 +0530

    Custom role check in routes,

--- nextapp/src/middleware.ts
+++ nextapp/src/middleware.ts
@@ -1,3 +1,27 @@
-export { default } from "next-auth/middleware";
+// Ref: https://next-auth.js.org/configuration/nextjs#advanced-usage
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(request: NextRequestWithAuth) {
+    // console.log(request.nextUrl.pathname);
+    // console.log(request.nextauth.token);
+    if (
+      request.nextUrl.pathname.startsWith("/create-user") &&
+      request.nextauth.token?.role !== "Admin"
+    ) {
+      // denied
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+// Applies next-auth only to matching routes - can be regex
+// Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = { matcher: ["/create-user"] };
